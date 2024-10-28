0cad5c7dd42fed38523843b78f71b3814eabaff1
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2024-10-28 12:16:53 +0200

    Some refactoring

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-echo 'adding execute permissions to enable-service...'
+echo "adding execute permissions to enable-service..."
 sudo chmod +x enable-service
-echo 'adding execute permissions to remove-service...'
+echo "adding execute permissions to remove-service..."
 sudo chmod +x remove-service
-echo 'adding exevute permissions to service-status...'
+echo "adding exevute permissions to service-status..."
 sudo chmod +x service-status
-echo 'moving enable-service to /usr/local/bin/...'
-sudo mv enable-service /usr/local/bin/
-echo 'moving remove-service to /usr/local/bin/...'
-sudo mv remove-service /usr/local/bin/
-echo 'moving service-status to /usr/local/bin/...'
-sudo mv service-status /usr/local/bin/
-echo 'done, you can now delete this setup file.'
-echo ''
-echo 'use enable-service <service> to enable a service.'
-echo 'use remove-service <service> to remove a service.'
-echo 'use service-status to check the services.'
+echo "copying enable-service to /usr/local/bin/..."
+sudo cp enable-service /usr/local/bin/
+echo "copying remove-service to /usr/local/bin/..."
+sudo cp remove-service /usr/local/bin/
+echo "copying service-status to /usr/local/bin/..."
+sudo cp service-status /usr/local/bin/
+echo "done!"
+echo ""
+echo "use enable-service <service> to enable a service"
+echo "use remove-service <service> to remove a service"
+echo "use service-status to check the services."
+echo ""
+echo "WARNING: Do not remove a service that you can't get back!"
